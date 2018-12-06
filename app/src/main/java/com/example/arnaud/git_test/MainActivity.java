28f9b6cfd69b2152b1cdad9f9e03abbdedd3a797
MelasseJoe <mugidu63@yahoo.fr>
2018-12-06 14:15:58 +0100

    Test number 2

--- app/src/main/java/com/example/arnaud/git_test/MainActivity.java
+++ app/src/main/java/com/example/arnaud/git_test/MainActivity.java
@@ -9,6 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // Test de changement de version sur GitHub
+
 
         setContentView(R.layout.activity_main);
     }
